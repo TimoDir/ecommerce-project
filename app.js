cc7f420e17feb2fb6dd92cf710a2780de295f00c
TimD <timothee.diroian@gmail.com>
2024-01-08 11:22:46 +0100

    Install bodyParser and cors Because I will need it later on

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const {getAllProducts, getProductById, addProduct} =  require('./db/querie');
 const port = 3000;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get('/', (req, res) => {
     res.send('Express app is working!')
