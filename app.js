bd8bcca4cc2c38af8ae46496cc37d6d4b0cf9457
TimD <timothee.diroian@gmail.com>
2024-01-08 12:41:18 +0100

    Setup the .env correctely and the DB + creation of models routes product inside the querie.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const {getAllProducts, getProductById, addProduct} =  require('./db/querie');
-const port = 3000;
+const {getAllProducts, getProductById, addProduct, deleteProduct} =  require('./db/querie');
+const {PORT} = require('./config');
+const port = PORT;
 
 app.use(bodyParser.json());
 app.use(
@@ -15,6 +16,12 @@ app.get('/', (req, res) => {
     res.send('Express app is working!')
 });
 
-  app.listen(port, ()=>{
+// Product routes
+app.get('/Products', getAllProducts);
+app.get('/Products/:id', getProductById);
+app.post('/Products/AddProduct', addProduct);
+app.delete('/Products/:id/deleteProduct', deleteProduct);
+
+app.listen(port, ()=>{
     console.log(`App is listening on port: ${port}`)
 });
--- db/querie.js
+++ db/querie.js
@@ -1,11 +1,63 @@
 const Pool = require('pg').Pool;
-require("dotenv").config();
+const { DB } = require('../config');
 
-const GITHUB_DB_PASSWORD = process.env.GITHUB_DB_PASSWORD;
+console.log(DB)
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'Ecommerces',
-  password: GITHUB_DB_PASSWORD,
-  port: 5432,
+  user: DB.PGUSER,
+  host: DB.PGHOST,
+  database: DB.PGDATABASE,
+  password: DB.PGPASSWORD,
+  port: DB.PGPORT
 });
+
+// Products
+
+const getAllProducts = (request, response) =>{
+    pool.query("SELECT * FROM products ORDER BY name ASC", (error, results) => {
+        if(error){
+            throw error;
+        };
+        response.status(200).json(results.rows)
+    });
+};
+
+const getProductById = (request, response) =>{
+    const id = parseInt(request.params.id);
+    pool.query(`SELECT * FROM products WHERE id =${id}`, (error, results) => {
+        if(error){
+            throw error;
+        };
+        response.status(200).json(results.rows)
+    });
+};
+
+const addProduct = (request, response) =>{
+    const {category, name, price, color} = request.body;
+
+    pool.query('INSERT INTO products (category, name, price, color) VALUES ($1, $2, $3, $4) RETURNING *', 
+        [category, name, price, color], 
+        (error, results) => {
+            if (error) {
+                throw error
+            };
+            response.status(201).send(`User added with ID: ${results.rows[0].id}`);
+    })
+};
+
+const deleteProduct = (request, response) =>{
+    const id = parseInt(request.params.id);
+    pool.query('DELETE FROM products WHERE id = $1', [id], (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).send(`User deleted with ID: ${id}`)
+      });
+};
+
+module.exports = {
+    getAllProducts,
+    getProductById,
+    addProduct,
+    deleteProduct
+};
